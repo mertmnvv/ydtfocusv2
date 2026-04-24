232e9aab1fa5a3eb21e3e0fa895068ae66c36604
Mert <mertyazilim3@gmail.com>
2026-04-24 23:17:11 +0300

    chore: ignore lint and ts errors during build to unblock vercel

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,15 +1,17 @@
+/* eslint-disable no-undef */
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
-firebase.initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyD41GCUqXJDryylH78RRov87vfVUk2GkWk",
   authDomain: "ydtfocus.firebaseapp.com",
   projectId: "ydtfocus",
   storageBucket: "ydtfocus.firebasestorage.app",
   messagingSenderId: "856274397620",
   appId: "1:856274397620:web:b2c5328d5cf85ce4eceae1"
-});
+};
 
+firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
@@ -17,7 +19,8 @@ messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/icon-512.png'
+    icon: '/icon-512.png',
+    badge: '/icon-192.png'
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
